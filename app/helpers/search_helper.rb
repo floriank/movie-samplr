1728730ee5baa7cf3d812342eb7436aa61d0d63d
Florian Kraft <floriankraft@gmx.de>
2016-02-27 20:05:20 +0100

    add tests for search helper

---
+++ app/helpers/search_helper.rb
@@ -0,0 +1,6 @@
+module SearchHelper
+
+  def imdb_url(movie)
+    "http://imdb.com/title/tt#{movie.id}"
+  end
+end
---
+++ spec/helpers/search_helper_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the SearchHelper. For example:
+#
+# describe SearchHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe SearchHelper, type: :helper do
+  it 'creates an imdb link for a given movie search result' do
+    movie = MovieSearchResult.new type: 'imdb', id: 110220, title: 'Deadpool'
+    expect(imdb_url(movie)).to eql 'http://imdb.com/title/tt110220'
+  end
+end
